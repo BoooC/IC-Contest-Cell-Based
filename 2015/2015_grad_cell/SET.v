5254e712bade249ef97f705243c2307d3711c8b9
Booo <p810105b@example.com>
2024-03-19 00:08:57 +0800

    upload

---
+++ 2015/2015_grad_cell/SET.v
@@ -0,0 +1,34 @@
+module SET ( 
+	input 				clk, 
+	input 				rst,
+	input 				en,
+	input 		[23:0] 	central,
+	input 		[11:0] 	radius,
+	input 		[1:0] 	mode,
+	output reg 			busy,
+	output 				valid,
+	output reg	[7:0]	candidate,
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+endmodule
+
